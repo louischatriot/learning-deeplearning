0522652ee847dc4e009c31d6b3cfce0389652c6c
Louis Chatriot <louis.chatriot@gmail.com>
2023-12-27 14:50:13 +0100

    Created my own stupidly inefficient version of k neighbors

--- digits/digits.py
+++ digits/digits.py
@@ -6,6 +6,8 @@ import matplotlib.pyplot as plt
 import matplotlib
 matplotlib.use('TkAgg')
 from skimage import transform
+import sys
+from collections import defaultdict
 
 digits = load_digits()
 (X_train, X_test, y_train, y_test) = train_test_split(digits.data, digits.target, test_size=0.25, random_state=42)
@@ -30,6 +32,50 @@ def display_image_and_result(X, y, idx):
     plt.imshow(image, interpolation='nearest')
     plt.show()
 
+
+class StupidModel:
+    def __init__(self, n_neighbors=4):
+        self.n_neighbors = n_neighbors
+
+    # "fit" that keeps all data in memory, which is very inefficient but OK for testing purposes
+    def fit(self, X, y):
+        self.data = X
+        self.target = y
+
+    # x is one element
+    def predict(self, x):
+        distances = []
+        for idx, d in enumerate(self.data):
+            distances.append((np.linalg.norm(x-d), idx))  # Using Euclidian norm as distance
+
+        distances.sort()
+
+        freq = defaultdict(lambda: 0)
+        for _, idx in distances[0:self.n_neighbors]:
+            freq[self.target[idx]] += 1
+
+        freq = [(v, k) for k, v in freq.items()]
+        freq.sort()
+
+        return freq[0][-1]
+
+
+
+model = StupidModel(n_neighbors=3)
+model.fit(X_train, y_train)
+
+for idx in range(10):
+    test_data = X_test[idx]
+    prediction = model.predict(test_data)
+    print(f"PREDICTION: {prediction}")
+    display_image_and_result(X_test, y_test, idx)
+
+
+
+
+sys.exit(0)
+
+# Below the code that uses the scikit learn model
 
 model = KNeighborsClassifier(n_neighbors=3)
 model.fit(X_train, y_train)
